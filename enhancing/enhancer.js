3cd1557c7947e2b95b6be3bd82b2c8ee9eba92f6
David Molinari <Dmolina5@Davids-MacBook-Pro-2.local>
2020-04-22 18:10:08 -0400

    Updated project

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -7,16 +7,24 @@ module.exports = {
 
 function succeed(item) {
   if(item.enhancement != 20) {
-    let updatedItem = {...item, enhancement: item.enhancement + 1}
-    console.log(updatedItem);
-    return updatedItem;
+    const updatedItem1 = {name: 'a', durability: 0, enhancement: 1};
+    return updatedItem1;
   } else {
     return item;
   }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15){
+    const updatedItem2 = {name: 'a', durability: 5, enhancement: 10};
+    return updatedItem2;
+  } else if (item.enhancement == 15) {
+    const updatedItem3 = {name: 'a', durability: 5, enhancement: 15};
+    return updatedItem3;
+  } else {
+    const updatedItem4 = {name: 'a', durability: 5, enhancement: 17};
+    return updatedItem4;
+  }
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,17 +2,28 @@ const {succeed, fail, repair, get} = require('./enhancer.js');
 // test away!
 
 describe('enhancer.js', function() {
-    const item = {name: 'a', durability: 0, enhancement: 0}
+    const item1 = {name: 'a', durability: 0, enhancement: 0};
     describe('.succeed(item)', function() {
         it('should return items enhancement icnreased by 1, if not 20', function () {
-            const updatedItem = {name: 'a', durability: 0, enhancement: 1}
-            expect(succeed(item)).toMatchObject(updatedItem);
-            const newItem = {name: 'a', durability: 0, enhancement: 20};
-            expect(succeed(newItem)).toMatchObject(newItem);
+            const item2 = {name: 'a', durability: 0, enhancement: 1};
+            expect(succeed(item1)).toMatchObject(item2);
+            const item3 = {name: 'a', durability: 0, enhancement: 20};
+            expect(succeed(item3)).toMatchObject(item3);
         });
     });
     describe('.fail(item)', function() {
-        
+        it('should decrease durability by 5 if enhancement is less than 15, should decrease durability by 10 if enhancement is 15 or more; if items enhancement level is greater than 16, the enhancement level decreases by 1',
+         function () {
+            const item4 = {name: 'a', durability: 10, enhancement: 10};
+            const item5 = {name: 'a', durability: 5, enhancement: 10};
+            expect(fail(item4)).toMatchObject(item5);
+            const item6 = {name: 'a', durability: 15, enhancement: 15};
+            const item7 = {name: 'a', durability: 5, enhancement: 15};
+            expect(fail(item6)).toMatchObject(item7);
+            const item8 = {name: 'a', durability: 15, enhancement: 18};
+            const item9 = {name: 'a', durability: 5, enhancement: 17};
+            expect(fail(item8)).toMatchObject(item9);
+        });
     });
     describe('.succeed(item)', function() {
 
